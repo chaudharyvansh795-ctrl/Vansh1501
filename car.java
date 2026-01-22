07a951f4fe02b07ed49d3be29bdc6bc3fb658c7b
chaudharyvansh795-ctrl <chaudharyvansh795@gmail.com>
2026-01-22 08:51:40 +0530

    Java Program to Create an Object

---
+++ car.java
@@ -0,0 +1,13 @@
+public class car {
+    int model,mileage;
+    public static void main(String[] args) {
+
+        car C1= new car();
+        car C2= new car();
+
+        System.out.println(C1);
+        System.out.println(C2);
+
+        
+    }
+}
