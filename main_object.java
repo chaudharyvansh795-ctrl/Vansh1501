5f4389b04fd07db0669dc1dd9520facd9beca72b
chaudharyvansh795-ctrl <chaudharyvansh795@gmail.com>
2026-01-22 08:52:30 +0530

    Java Program to Create an Object

---
+++ main_object.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class main_object {
+
+    int n;
+    int sum = 0;
+
+    public static void main(String[] args) {
+
+        main_object m = new main_object();
+        Scanner s = new Scanner(System.in);
+
+        m.n = s.nextInt();
+    }
+}
