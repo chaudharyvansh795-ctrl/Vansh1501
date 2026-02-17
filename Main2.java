a00ca6487381727c21e6f1b7fbef4f6160a997a0
chaudharyvansh795-ctrl <chaudharyvansh795@gmail.com>
2026-02-17 20:10:55 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,13 @@
+package package2;
+
+import package1.Car;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        System.out.println("Mileage: " + c.mileage);
+        System.out.println("Price: " + c.price);
+    }
+}
